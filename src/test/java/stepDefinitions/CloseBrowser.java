1ba640b5a125aeaf43039579965bd7e16cfb12a9
US\sanpatnaik <sanpatnaik@deloitte.com>
2018-11-02 20:19:03 +0530

    Dividing Step Files

---
+++ src/test/java/stepDefinitions/CloseBrowser.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package stepDefinitions;
+import static org.junit.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import cucumber.TestContext;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import managers.WebDriverManager;
+import pageObjects.HomePage;
+import cucumber.TestContext;
+
+/**
+ * @author sanpatnaik
+ *
+ */
+public class CloseBrowser {
+	TestContext testContext;
+	WebDriver Driver;
+	public CloseBrowser(TestContext context) {
+		testContext = context;
+		Driver=testContext.getWebDriverManager().getDriver();
+	}
+	@And("^close the web browser$")
+	public void tearDownWebDriver() {
+		Driver.quit();
+	}
+
+}
