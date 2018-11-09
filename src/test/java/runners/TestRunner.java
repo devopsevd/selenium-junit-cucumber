9b62b8dcea358dc996554b12218a359cc203fcbf
US\sanpatnaik <sanpatnaik@deloitte.com>
2018-11-09 10:42:48 +0530

    Final Deploy- Nov-9

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -14,13 +14,18 @@ import managers.FileReaderManager;
 		/*plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json",
 				"junit:target/cucumber-reports/Cucumber.xml",
 				"html:target/cucumber-reports"},*/
-		plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json", 
-				"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
+		plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"},
 		monochrome = true
 		)
 public class TestRunner {
 	@AfterClass
 	public static void writeExtentReport() {
 		Reporter.loadXMLConfig(new File(FileReaderManager.getInstance().getConfigReader().getReportConfigPath()));
+		Reporter.setSystemInfo("User Name", System.getProperty("user.name"));
+	    Reporter.setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+	    Reporter.setSystemInfo("Machine", 	"Windows 10" + "64 Bit");
+	    Reporter.setSystemInfo("Selenium", "3.7.0");
+	    Reporter.setSystemInfo("Maven", "3.5.2");
+	    Reporter.setSystemInfo("Java Version", "1.8.0_151");
 	}
 }
--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -2,7 +2,14 @@
  * 
  */
 package stepDefinitions;
+import java.io.File;
+import java.io.IOException;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import com.cucumber.listener.Reporter;
+import com.google.common.io.Files;
 import cucumber.TestContext;
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
@@ -12,13 +19,15 @@ import cucumber.api.java.Before;
  */
 public class Hooks {
 	TestContext testContext;
-	 
+
 	public Hooks(TestContext context) {
 		testContext = context;
 	}
- 
+
 	@Before
 	public void BeforeSteps() {
+		//Reporter.addStepLog(“Step Log message goes here”);
+		Reporter.assignAuthor("Demo ToolKit - Sandeep Patnaik");
 		/*What all you can perform here
 			Starting a webdriver
 			Setting up DB connections
@@ -26,10 +35,31 @@ public class Hooks {
 			Setting up browser cookies
 			Navigating to certain page
 			or anything before the test
-		*/
+		 */
 	}
- 
-	@After
+	@After(order = 1)
+	public void afterScenario(Scenario scenario) {
+		if (scenario.isFailed()) {
+			String screenshotName = scenario.getName().replaceAll(" ", "_");
+			try {
+				//This takes a screenshot from the driver at save it to the specified location
+				File sourcePath = ((TakesScreenshot) testContext.getWebDriverManager().getDriver()).getScreenshotAs(OutputType.FILE);
+
+				//Building up the destination path for the screenshot to save
+				//Also make sure to create a folder 'screenshots' with in the cucumber-report folder
+				File destinationPath = new File(System.getProperty("user.dir") + "/target/cucumber-reports/screenshots/" + screenshotName + ".png");
+
+				//Copy taken screenshot from source location to destination location
+				Files.copy(sourcePath, destinationPath);   
+
+				//This attach the specified screenshot to the test
+				Reporter.addScreenCaptureFromPath(destinationPath.toString());
+			} catch (IOException e) {
+			} 
+		}
+	}
+
+	@After(order = 0)
 	public void AfterSteps() {
 		testContext.getWebDriverManager().getDriver().quit();
 	}
